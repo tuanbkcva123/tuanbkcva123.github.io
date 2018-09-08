00392b0860ceb825fddf52063b37459a9c230970
Tuan Nguyen <Tuan1381@gmail.com>
2018-09-08 17:37:56 +0700

    menory card

---
+++ Memory card/card.js
@@ -0,0 +1,102 @@
+$(function() {
+    $('#btn_clk').click(function() {
+        $('#playbtn').addClass('hd')
+    })
+
+    $('#btn_clk_main').click(function() {
+        $('#playbtn').addClass('hd')
+    })
+
+
+    
+    
+    $('.c1').click(function() {
+        $('.f1').addClass('front_clk')
+        $('.b1').addClass('back_clk')
+    })
+
+    $('.c2').click(function() {
+        $('.f2').addClass('front_clk')
+        $('.b2').addClass('back_clk')
+    })
+
+    $('.c3').click(function() {
+        $('.f3').addClass('front_clk')
+        $('.b3').addClass('back_clk')
+    })
+
+    $('.c4').click(function() {
+        $('.f4').addClass('front_clk')
+        $('.b4').addClass('back_clk')
+    })
+
+    $('.c5').click(function() {
+        $('.f5').addClass('front_clk')
+        $('.b5').addClass('back_clk')
+    })
+
+    $('.c6').click(function() {
+        $('.f6').addClass('front_clk')
+        $('.b6').addClass('back_clk')
+    })
+
+    $('.c7').click(function() {
+        $('.f7').addClass('front_clk')
+        $('.b7').addClass('back_clk')
+    })
+
+    $('.c8').click(function() {
+        $('.f8').addClass('front_clk')
+        $('.b8').addClass('back_clk')
+    })
+
+    $('.c9').click(function() {
+        $('.f9').addClass('front_clk')
+        $('.b9').addClass('back_clk')
+    })
+
+    $('.c10').click(function() {
+        $('.f10').addClass('front_clk')
+        $('.b10').addClass('back_clk')
+    })
+
+    $('.c11').click(function() {
+        $('.f11').addClass('front_clk')
+        $('.b11').addClass('back_clk')
+    })
+
+    $('.c12').click(function() {
+        $('.f12').addClass('front_clk')
+        $('.b12').addClass('back_clk')
+    })
+
+    $('.c13').click(function() {
+        $('.f13').addClass('front_clk')
+        $('.b13').addClass('back_clk')
+    })
+
+    $('.c14').click(function() {
+        $('.f14').addClass('front_clk')
+        $('.b14').addClass('back_clk')
+    })
+
+    $('.c15').click(function() {
+        $('.f15').addClass('front_clk')
+        $('.b15').addClass('back_clk')
+    })
+
+    $('.c16').click(function() {
+        $('.f16').addClass('front_clk')
+        $('.b16').addClass('back_clk')
+    })
+
+    $('.c17').click(function() {
+        $('.f17').addClass('front_clk')
+        $('.b17').addClass('back_clk')
+    })
+
+    $('.c18').click(function() {
+        $('.f18').addClass('front_clk')
+        $('.b18').addClass('back_clk')
+    })
+})
